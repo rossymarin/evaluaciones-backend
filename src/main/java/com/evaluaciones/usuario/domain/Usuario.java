d5e9c66121e12da3ef13bd43e20d1b867f46c30c
rossymarin <rosymarin2431@gmail.com>
2023-04-17 13:26:30 -0600

    feat: Implementacion jwt y servicio generate-token

--- src/main/java/com/evaluaciones/usuario/domain/Usuario.java
+++ src/main/java/com/evaluaciones/usuario/domain/Usuario.java
@@ -1,17 +1,23 @@
 package com.evaluaciones.usuario.domain;
 
+import com.evaluaciones.security.domain.Auth;
 import com.evaluaciones.usuarioRol.domain.UsuarioRol;
-import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "usuarios")
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,5 +40,30 @@ public class Usuario {
     private String perfil;
 
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "usuario")
+    @JsonIgnore
     private Set<UsuarioRol> usuarioRoles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<Auth> autoridades = new HashSet<>();
+        this.usuarioRoles.forEach(usuarioRol -> {
+            autoridades.add(new Auth(usuarioRol.getRol().getNombre()));
+        });
+        return autoridades;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 }
